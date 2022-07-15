cea74d620fb1ae2521d3860bd6b476f80d4af3a6
jolitti <marcodeltrionfo@gmail.com>
2022-07-15 16:57:48 +0200

    Added moving square

---
+++ Assets/BasicControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicControl : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private GameObject thisObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisObject = this.gameObject;
+        Debug.Log("Game start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        thisObject.transform.position += (Vector3) input * speed;
+    }
+}
